a0ad2c878a59d6ad92b48e24388ff22a0d7ec640
Sam Harris <sam@jam-bc.com>
2019-08-13 13:05:14 +0100

    1, 2 complete and cookie decoding

--- exercise-1/router.js
+++ exercise-1/router.js
@@ -22,6 +22,40 @@ module.exports = (req, res) => {
           return res.end(data);
         }
       );
+      break;
+
+    case 'POST /login':
+      jwt.sign({user: "Sam"}, 'SecretStuff', {algorithm: 'HS256'}, function(err, token) {
+        if (err) {
+          return err;
+        } else {
+          res.writeHead(302, {'Set-Cookie': `loginCookie=${token}; logged_in=true; Max-Age=9000;`, 'Location': '/'});
+          return res.end(token);
+        }
+      });
+      break;
+
+    case 'POST /logout':
+      res.writeHead(302, {'Set-Cookie': `${req.headers.cookie}; logged_in=false;  Max-Age=0;`, 'Location': '/'});
+      return res.end();
+      break;
+
+    case 'GET /auth_check':
+      const cookies = cookie.parse(req.headers.cookie || '');
+
+      if (typeof cookies.loginCookie != 'undefined') {
+        var decoded = jwt.verify(cookies.loginCookie, 'SecretStuff')
+        console.log(decoded.user);
+        
+      }
+      // const loginCookie = cookies.loginCookie;
+
+
+
+      // console.log('our loginCookie value = ', loginCookie);
+      // console.log('cookies = ', cookies);
+
+
     default:
       res.writeHead(
         404,
